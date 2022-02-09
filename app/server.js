7f88c3cad5b0bb48d750f95abc0892f0fc069453
anilkumarmaheswarla <49440986+anilkumarmaheswarla@users.noreply.github.com>
2022-02-09 10:35:02 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome to DevOps. My First CI/CD Pipeline!!\n');
+  res.send('Welcome to DevOps. Hello, this is my First CI/CD Pipeline!!\n');
 });
 
 app.listen(PORT, HOST);
